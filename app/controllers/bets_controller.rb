72f278c32193ad648706f78e19f7a1c8e967a5f7
celtic93 <egorka80@list.ru>
2022-07-31 10:55:43 +0300

    Added Bet scopes

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,5 +1,5 @@
 class BetsController < ApplicationController
   def index
-    @bets = Bet.all
+    @bets = Bet.ended.order_by_tournament_and_started_at
   end
 end
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,4 +1,7 @@
 class Bet < ApplicationRecord
   validates :event_name, :tournament, :market, :market_details, :coefficient, :overvalue, :started_at, presence: true
   validates :event_name, uniqueness: { scope: :started_at }
+
+  scope :ended, -> { where('started_at < ?', 3.hours.ago) }
+  scope :order_by_tournament_and_started_at, -> { order(:tournament, :started_at) }
 end
