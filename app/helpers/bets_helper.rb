5b4930966cdb1a08e2ebe638875a17b331016cd9
celtic93 <egorka80@list.ru>
2022-08-04 00:37:43 +0300

    Added google_search_link

--- app/helpers/bets_helper.rb
+++ app/helpers/bets_helper.rb
@@ -1,2 +1,5 @@
 module BetsHelper
+  def google_search_link(event_name)
+    link_to event_name, "https://www.google.com/search?q=#{event_name}", target: '_blank', rel: 'noopener noreferrer'
+  end
 end
