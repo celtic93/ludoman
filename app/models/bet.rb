e4bc92820ee5fcef5ec181b93adddf478bb7ea9c
celtic93 <egorka80@list.ru>
2022-08-04 10:07:06 +0300

    Fixed ENOUGH_TIME_FOR_SPORT_EVENT constant

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,11 +1,11 @@
 class Bet < ApplicationRecord
-  ENOUGH_TIME_FOR_SPORT_EVENT = 3.hours.ago
+  ENOUGH_TIME_FOR_SPORT_EVENT = 3.hours
 
   validates :event_name, :tournament, :sport, :market, :market_details,
             :coefficient, :overvalue, :started_at, presence: true
   validates :event_name, uniqueness: { scope: :market }
 
-  scope :ended, -> { where('started_at < ?', ENOUGH_TIME_FOR_SPORT_EVENT) }
+  scope :ended, -> { where('started_at < ?', ENOUGH_TIME_FOR_SPORT_EVENT.ago) }
   scope :order_by_event_info, -> { order(:sport, :tournament, :event_name, :started_at) }
 
   enumerize :result, in: %i[pending won lost draw], predicates: true, scope: true
