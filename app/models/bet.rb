b3dcd51c7a49ad275d7ca84331c7e860fb9b1843
celtic93 <egorka80@list.ru>
2022-08-07 20:02:59 +0300

    Added overvalue validation

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -4,6 +4,7 @@ class Bet < ApplicationRecord
   validates :event_name, :tournament, :sport, :market, :market_details,
             :coefficient, :overvalue, :started_at, presence: true
   validates :event_name, uniqueness: { scope: :market }
+  validates :overvalue, numericality: { greater_than_or_equal_to: 7 }
 
   scope :ended, -> { where('started_at < ?', ENOUGH_TIME_FOR_SPORT_EVENT.ago) }
   scope :order_by_event_info, -> { order(:sport, :tournament, :event_name, :started_at) }
